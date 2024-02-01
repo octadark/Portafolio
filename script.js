ccce6b3b4496a6535454c5f73dcc3b05a2d9ba4e
octadark <octaviochavez154@gmail.com>
2024-01-31 21:48:13 -0300

    Actualizacion #2

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+//declarar variable
+const toggle = document.getElementById('toggledark');
+const body = document.querySelector('body');
+
+//adjuntando controlador de eventos
+toggle.addEventListener('click', function(){
+  this.classList.toggle('bi-moon');
+  if(this.classList.toggle('bi-brightness-high-fill')){
+    //modo claro
+    document.body.style.background = "linear-gradient(135deg, #0c036a, #06a8ff)";
+    body.style.color = 'white';
+  }else{
+    //modo oscuro
+    document.body.style.background = "linear-gradient(150deg, #000000, #454545)";
+    body.style.color = 'white';
   }
+})
